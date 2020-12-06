f2881214ec00899f69f57d0be83adbe53bd16e42
AlexHaxe <Alexander.Blum@gmail.com>
2020-12-06 18:56:56 +0100

    added unittest for case capture

--- test/refactor/ScopedLocalTest.hx
+++ test/refactor/ScopedLocalTest.hx
@@ -7,14 +7,13 @@ class ScopedLocalTest extends TestBase {
 		setupData(["testcases/scopedlocal"]);
 	}
 
-	public function testRenameSomeFunction() {
+	public function testRenameContextParameter() {
 		var edits:Array<TestEdit> = [
 			makeReplaceTestEdit("testcases/scopedlocal/Refactor.hx", "refactorContext", 498, 505),
 			makeReplaceTestEdit("testcases/scopedlocal/Refactor.hx", "refactorContext.fileList.getFile", 564, 588),
 			makeReplaceTestEdit("testcases/scopedlocal/Refactor.hx", "refactorContext.what.fileName", 589, 610),
 			makeReplaceTestEdit("testcases/scopedlocal/Refactor.hx", "refactorContext.what.pos", 709, 725),
 			makeReplaceTestEdit("testcases/scopedlocal/Refactor.hx", "refactorContext.what.toName", 805, 824),
-
 			makeReplaceTestEdit("testcases/scopedlocal/Refactor.hx", "refactorContext", 953, 960),
 			makeReplaceTestEdit("testcases/scopedlocal/Refactor.hx", "refactorContext", 1084, 1091),
 			makeReplaceTestEdit("testcases/scopedlocal/Refactor.hx", "refactorContext", 1242, 1249),
@@ -24,5 +23,13 @@ class ScopedLocalTest extends TestBase {
 
 		];
 		refactorAndCheck({fileName: "testcases/scopedlocal/Refactor.hx", toName: "refactorContext", pos: 504}, edits);
+	}
+
+	public function testRenameScopeEndCaseCapture() {
+		var edits:Array<TestEdit> = [
+			makeReplaceTestEdit("testcases/scopedlocal/Refactor.hx", "endPos", 2016, 2024),
+			makeReplaceTestEdit("testcases/scopedlocal/Refactor.hx", "endPos", 2098, 2106),
+		];
+		refactorAndCheck({fileName: "testcases/scopedlocal/Refactor.hx", toName: "endPos", pos: 2020}, edits);
 	}
 }
